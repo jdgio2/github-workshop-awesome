b608bb88c63fb7b71a05b67d0de184afbee7cb52
braidenlidyoff <blidyoff7@gmail.com>
2025-03-26 18:30:17 -0700

    Changes1

--- new.cpp
+++ new.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main()
 {
+    cout << "We jawn" << endl;
+    cout << "We oking on this jawn" << endl;
+    cout << "We are cooking " << endl;
     cout << "We are cooking on this jawn" << endl;
+    cout << " on this jawn" << endl;
     cout << "We are cooking on this jawn" << endl;
-    cout << "We are cooking on this jawn" << endl;
-    cout << "We are cooking on this jawn" << endl;
-    cout << "We are cooking on this jawn" << endl;
-    cout << "We are cooking on this jawn" << endl;
-    cout << "We are cooking on this jawn" << endl;
+    cout << "We are on this jawn" << endl;
+    cout << "AAAAAHHHHHHHHH" << endl;
 
     cout << "hello there" << endl;
 
