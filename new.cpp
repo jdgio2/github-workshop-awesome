979fb1a8bfc1576e012dabe2942e71a0c9c0c9ff
JD Giovannetti <jonathangiovannetti2@gmail.com>
2025-03-26 18:13:37 -0700

    I madde a new message

--- new.cpp
+++ new.cpp
@@ -11,6 +11,8 @@ int main()
     cout << "We are cooking on this jawn" << endl;
     cout << "We are cooking on this jawn" << endl;
 
+    cout << "hello there" << endl;
+
     cout << "Hello world!" << endl;
     return 0;
 }
